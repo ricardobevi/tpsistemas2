72d9516b5d67e706e54a9e3f181bf849ef167f05
facu22.14@gmail.com <facu22.14@gmail.com@4bd5a78b-6969-a388-ad65-09c7baf7c938>
2010-06-09 04:18:49 +0000

    TP3 -Ej 2 Version corregida del script funcionando, y comentada

--- TP3/codigo/Ejercicio 2 - password/generador-de-pass.sh
+++ TP3/codigo/Ejercicio 2 - password/generador-de-pass.sh
@@ -10,37 +10,38 @@
 # Marcela ....
 # Moure Pablo          32.031.459
 #
-# comienzo del script:
+# COMIENZO DEL SCRIPT:
 
 
 # corroboro que se envie un solo parametro 
 if [ ! $1 ]
 then  
-	echo "ERROR: este script utiliza un solo parametro \n USO: \t  `basename $0 ` password"	
-	exit
+        echo -e "\nERROR:\n\t este script utiliza un solo parametro\n"
+        echo -e "USO:\t `basename $0 ` password \n"        
+        exit 0
 fi
 
 
+# Asigno valor de entrada a la variable pass para trabajarla desde alli,
+# y la longitud dada en archivo de configuracion para la longitud minima del pass a longmin
 
-pass=$1  #asigno valor de entrada a la variable pass para trabajarla desde alli
-
-
-
-#corroboro que el pass sea mayor de la longitud minima establecida en el archivo pasado por parametro
-# si el awk devuelve un 1, indica caso contrario ( menor a la longitud minima ), entonces cierro script
-
+pass=$1  
 longmin=`cat pass-configure | cut -d"=" -f2`
-awk -v pass=$pass -v longmin=$longmin -f pass-awk pass-constantes
-  
-  if test $? -eq 1 
+
+# Corroboro que la cantidad de caracteres de pass sea mayor de la longitud minima establecida 
+# si el awk devuelve un 1, indica caso contrario ( menor a la longitud minima )
+# entonces Informo y cierro script
+
+  if test `echo $pass $longmin | awk  '{ if ( length($1) < $2) print 1  }'` 
     then
-    exit 1
+        echo -e "\nERROR:\n\t El pass es menor al tamaño minimo establecido para una contrasenia\n"
+        echo -e "\t Tamaño minimo:\t $longmin \n"   
+        exit 0
   fi  
 
 
 #paso la primer letra a minuscula en caso de que sea una mayuscula
 pass=`echo $pass | sed -r 's/(^[A-Z])/\L&/g'`
-
 
 
 #realizo el reemplazo de variables
@@ -52,16 +53,20 @@ do
          # funcionamiento sin considerar regla de dos consonantes ( punto c )
          #                       pass=`echo $pass | sed -e 's/'"$consonante"'/'"$valor"'/g'`
          #
-         # Para separar la expresion en sectores:
+         # Para separar la expresion en sectores y poder realizar el punto c:
          #              $ sed -e 's/\(.*\) \(.*\) \(.*\)/Victor \1-\2 Von \3/' miarchivo.txt
 
          pass=`echo $pass | sed -e 's/'"$consonante"'/'"$valor"'/g'`
          pass=`echo $pass | sed -e 's/\([0123456789]\)\('"$valor"'\)/\1'"$consonante"'/g'`
-         echo $pass > password-out
+        
 
 done
 
-exit 1;
+#guardo pass en archivo ,lo informo y termino script
+echo -e "\nEl password codificado es:  "$pass
+echo $pass > password-out
+
+exit 0;
 
                                             ###############
                                             #     END     #
