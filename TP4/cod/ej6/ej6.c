cb2019602690c15d2c5d2bf04977ab8620f911f8
facu22.14 <facu22.14@gmail.com>
2010-06-29 07:35:42 +0000

    TP4 -Ej 6 VERSION FINAL sin debug

--- TP4/cod/ej6/ej6.c
+++ TP4/cod/ej6/ej6.c
@@ -14,6 +14,7 @@ int main ()
     char user[50];
     int i = 0;
     pid_t pidDelEjecutable;
+    int cantBkground = 0;
     struct arg argumentos;
 
     chdir( getenv("HOME") );
@@ -31,34 +32,47 @@ int main ()
 
           while( strcmp(linea,"exit") )
           {
-                  if( strcmp(linea,"") && strcmp(linea,"cd") && strcmp(linea,"echo") ) 
-                  {             
-                          if ( ( pidDelEjecutable = fork() ) == -1 ) 
-                          {
-                                  perror("Error al crear el hijo");
-                                  exit(1);
-                          }
+                  if( strcmp(linea,"")  && strcmp(linea,"echo") ) 
+                  {         
+                              split_args(linea, &argumentos);
+                              printf("\n\n%d\n\n",argumentos.bkground);
 
-                          else if( pidDelEjecutable == 0 )
-                          {       
-                                  split_args(linea, &argumentos);
-                                  
-                                  if( execvp( argumentos.argum[0], argumentos.argum ) < 0 ) {
-                                        perror("   Error");
-                                        exit(1);
-                                  }
-                          }
-                          else
-                          {
-                                  wait(NULL);
-                          }
+                              if ( !strcmp( argumentos.argum[0] ,"cd") )
+                              {
+                                    chdir(argumentos.argum[1]);
+                                    getcwd(dirAct, 300);
+                              }
+                              else if ( ( pidDelEjecutable = fork() ) == -1 ) 
+                              {
+                                      perror("Error al crear el hijo");
+                                      exit(1);
+                              }
+
+                              else if( pidDelEjecutable == 0 )
+                              {        
+                                      if(argumentos.crear)
+                                                  freopen(argumentos.arch, "w", stdout);
+                                      else if (argumentos.agregar)
+                                                  freopen(argumentos.arch, "a", stdout);
+
+                                      if( execvp( argumentos.argum[0], argumentos.argum ) < 0 ) {
+                                            perror("   Error");
+                                            exit(1);
+                                      }
+                              }
+                              else
+                              {
+
+                                      if ( ! argumentos.bkground )
+
+                                                   waitpid(pidDelEjecutable);
+                                      else
+                                                    wait(NULL);
+
+                                   
+                              }
 
                    }     
-                   else if ( !strcmp(linea,"cd") )
-                   {
-                         chdir("/");
-                         getcwd(dirAct, 300);
-                   }
                    else if ( !strcmp(linea,"echo") ) 
                    {
                         printf("%s",linea);
@@ -67,7 +81,16 @@ int main ()
               printf("%s(%s):%s=> ",user ,host, dirAct);
               fgets(linea, 300, stdin);
               linea[strlen(linea) - 1] = '\0';
-          }     
+          }    
+    
+/* 
+//  aparentemente con el waitpid no hay necesidad de este for   
+
+    for(i=0; i< cantBkground ; i++)
+    {
+        wait();
+    }
+*/
     
     return 0;
 }
