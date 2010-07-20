44bba79cf9002eb494de0a5cfa4259562bf1d8c6
rus.spes <rus.spes@4bd5a78b-6969-a388-ad65-09c7baf7c938>
2010-07-20 02:59:03 +0000

    [TP5] Ej4 terminado, faltan comentarios.

---
+++ TP5/cod/ej4/ej4-cliente.c
@@ -0,0 +1,100 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <signal.h>
+#include <fcntl.h>
+#include <stdlib.h>
+
+pid_t serverPid;
+char *archivoOut;
+int fifo,
+    nums[100],
+    cant = 0,
+    max = 0,
+    min = 0,
+    sum = 0;
+
+void cSIGUSR1(int iNumSen, siginfo_t *info, void *ni) {
+  serverPid = info->si_pid;
+}
+
+void cSIGUSR2(int iNumSen, siginfo_t *info, void *ni) {
+    read(fifo, (int*) &nums[cant], sizeof(int) );
+    sum += nums[cant];
+    
+    if ( cant == 0 )
+        max = min = nums[cant];
+    
+    if ( nums[cant] > max )
+        max = nums[cant];
+    else
+        min = nums[cant];
+    
+    cant++;
+}
+
+void cTERM(int iNumSen, siginfo_t *info, void *ni) {
+    int i = 0;
+    
+    FILE * arch = fopen(archivoOut, "w");
+    fprintf( arch, "Maximo = %d\n"
+                   "Minimo = %d\n"
+                   "Promedio = %.2f\n", max, min, (float) sum / cant );
+    
+    for( i = 0 ; i < cant ; i++ )
+        fprintf( arch, "%d\n", nums[i] );
+    
+    fclose(arch);
+    printf("Terminado Cliente\n");
+    kill(serverPid, SIGTERM);
+    exit(0);
+}
+
+int main( int argc, char* argv[] ){
+    pid_t miPid = 0;
+    struct sigaction usr1,
+                     usr2,
+                     term;
+    
+    int i = 0;
+    
+    daemon(1, 1);
+    
+    usr1.sa_sigaction = cSIGUSR1;
+    usr2.sa_sigaction = cSIGUSR2;
+    term.sa_sigaction = cTERM;
+    
+    sigfillset( &usr1.sa_mask );
+    sigfillset( &usr2.sa_mask );
+    sigfillset( &term.sa_mask );
+    
+    usr1.sa_flags = SA_SIGINFO;
+    usr2.sa_flags = SA_SIGINFO;
+    term.sa_flags = SA_SIGINFO | SA_NODEFER;
+    
+    sigaction(SIGTERM, &term, NULL);
+    
+    if (argc == 3){
+        
+        if ( ( fifo = open ( argv[1], O_RDWR ) ) < 0 ){
+            perror("Error FIFO: ");
+            return -1;
+        }
+        
+        archivoOut = argv[2];
+        
+        miPid = getpid();
+        
+        write(fifo, (int*) &miPid, sizeof(pid_t));
+        
+        sigaction(SIGUSR1, &usr1, NULL);
+        sigaction(SIGUSR2, &usr2, NULL);
+        
+        while(1);
+        
+    }else{
+        printf("Mal Argumentos\n");
+    }
+    
+    return 0;
+}
---
+++ TP5/cod/ej4/ej4-servidor.c
@@ -0,0 +1,72 @@
+#include<stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <signal.h>
+#include <stdlib.h>
+#include <fcntl.h>
+
+pid_t cons[2];
+
+void cTERM(int iNumSen, siginfo_t *info, void *ni) {
+    printf("Terminado Servidor\n");
+    kill(cons[0], SIGTERM);
+    kill(cons[1], SIGTERM);
+    exit(0);
+}
+
+int main( int argc, char* argv[] ){
+    int i = 0,
+        num = 0,
+        fifo = 0;
+        
+    struct sigaction term;
+    
+    term.sa_sigaction = cTERM;
+    sigfillset( &term.sa_mask );
+    term.sa_flags = SA_SIGINFO | SA_NODEFER;
+    sigaction(SIGTERM, &term, NULL);
+    sigaction(SIGINT, &term, NULL);
+    
+    if (argc == 2){
+        mkfifo(argv[1], 0660);
+        fifo = open ( argv[1], O_RDWR );
+        
+        printf("Esperando clientes...\n");
+        for(i = 0 ; i < 2 ; i++){
+            read(fifo, (int*) &cons[i], sizeof(pid_t));
+            printf("Se conecto el cliente %d con PID %d\n", i + 1, cons[i]);
+        }
+        
+        kill(cons[0], SIGUSR1);
+        kill(cons[1], SIGUSR1);
+        
+        printf("Ingrese numeros enteros.\n-> ");
+        
+        while (1){
+        
+            scanf("%d",&num);
+            
+            write ( fifo, (int*) &num, sizeof(int) );
+            
+            if ( num % 2 == 0 ) {
+                               
+                kill(cons[0], SIGUSR2);
+                
+                printf("Numero PAR enviado a 1.\n-> ");
+                
+            }else{
+                
+                kill(cons[1], SIGUSR2);
+                
+                printf("Numero IMPAR enviado a 2.\n-> ");
+                
+            }
+        }
+    }else{
+        
+        printf("Mal Argumentos\n");
+        
+    }
+    
+    return 0;
+}
