b104487287c28018e2fc7c02e93dfc3df03d3291
rus.spes <rus.spes@4bd5a78b-6969-a388-ad65-09c7baf7c938>
2010-08-25 20:18:45 +0000

    [TP6] ej4 hecho por marce correjido y con semaforo.h diferente al de los ejercicios 2 y 3. Todo con header.

---
+++ TP6/cod/ej4/semaforo.h
@@ -0,0 +1,65 @@
+/***********************************************/
+/* Nombre: semaforo.h                          */
+/* Trabajo: TP6 Semaforos y Memoria Compartida */
+/* Numero de ejercicio: 4                      */
+/* Entrega: Primer Entrega                     */
+/*                                             */
+/* Grupo N 63                                  */
+/* D'Aranno Facundo      34.842.320            */
+/* Jose Ferreyra         31.144.004            */
+/* Marcela A. Uslenghi   26.920.315            */
+/* Bevilacqua Ricardo    34.304.983            */
+/***********************************************/
+
+
+
+#ifndef SEMAFORO_H
+#define SEMAFORO_H
+
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/sem.h>
+
+union semun{
+    int val;
+    struct semid_ds *buf;
+    unsigned short *array;
+}arg;
+
+
+int obtenerSemaforo(key_t clave, int valor) {
+   int IdSemaforo;
+   union semun CtlSem;
+   IdSemaforo = semget(clave, 1, IPC_CREAT | 0600);
+   CtlSem.val = valor;
+   semctl(IdSemaforo, 0, SETVAL, CtlSem);
+   return IdSemaforo;
+}
+
+void pedirSemaforo(int IdSemaforo) {
+   struct sembuf OpSem;
+   OpSem.sem_num = 0;
+   OpSem.sem_op = -1;   /* Decrementa semval o bloquea si cero */
+   OpSem.sem_flg = 0;
+   semop(IdSemaforo, &OpSem, 1);
+}
+
+void devolverSemaforo(int IdSemaforo) {
+   struct sembuf OpSem;
+   OpSem.sem_num = 0;
+   OpSem.sem_op = 1;    /* Incrementa en 1 el semáforo */
+   OpSem.sem_flg = 0;
+   semop(IdSemaforo, &OpSem, 1);
+}
+
+void eliminarSemaforo(int IdSemaforo) {
+   semctl(IdSemaforo, 0, IPC_RMID);
+}
+
+#endif
+
+
+
+/*******/
+/* FIN */
+/*******/
