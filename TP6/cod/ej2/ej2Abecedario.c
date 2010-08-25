f203e35c754d043b9a22fd6d538a68587769a528
rus.spes <rus.spes@4bd5a78b-6969-a388-ad65-09c7baf7c938>
2010-08-25 20:16:45 +0000

    [TP6] ej2 totalemente comentado y con header, tambien se comento semaforo.h.

--- TP6/cod/ej2/ej2Abecedario.c
+++ TP6/cod/ej2/ej2Abecedario.c
@@ -1,3 +1,18 @@
+/***********************************************/
+/* Nombre: ej2Abecedario.c                     */
+/* Trabajo: TP6 Semaforos y Memoria Compartida */
+/* Numero de ejercicio: 2                      */
+/* Entrega: Primer Entrega                     */
+/*                                             */
+/* Grupo N 63                                  */
+/* D'Aranno Facundo      34.842.320            */
+/* Jose Ferreyra         31.144.004            */
+/* Marcela A. Uslenghi   26.920.315            */
+/* Bevilacqua Ricardo    34.304.983            */
+/***********************************************/
+
+
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/wait.h>
@@ -25,6 +40,11 @@ int main(){
     key_t clave;
     
     clave = ftok(".", 'R');
+    
+    /*
+     * Creo los semaforos que voy a utilizar, en este caso, al tener que lograr
+     * una alternancia estricta, utilizo 2.
+     */
 
     if ( ( semaforo = semget(clave, 2, IPC_CREAT | IPC_EXCL | 0660 ) ) == -1 ){
         perror("main: semget");
@@ -34,15 +54,31 @@ int main(){
         exit(1);
     }
     
+    /*
+     * Seteo un semaforo con valor 0 y otro con valor 1, asegurandome asi
+     * que empiece por un proceso determinado.
+     */
+    
     if ( setSem(semaforo, 0, 1) ) 
         exit(1);
     
     if ( setSem(semaforo, 1, 0) ) 
         exit(1);
     
+    /*
+     * Creo mis dos hijos. Con este while puedo crear mis hijos a partir del
+     * padre y no me quedan mas generaciones (no quedan nietos, etc.). Luego
+     * analizando el valor con que queda la variable "i" puedo definir a cada
+     * uno de los hijos y al padre.
+     */
+    
     while( i < 2 && ( childPids[i] = fork() ) != 0 ) i++;
     
     if(i == 2){/* Padre */
+        
+        /*
+         * El padre espera por sus dos hijos, remueve el semaforo y termina.
+         */
         
         wait(NULL);
         wait(NULL);
@@ -50,9 +86,19 @@ int main(){
         rmSem(semaforo);
         
     }else if ( childPids[ i ] == 0 ){/* Hijos */
+        
+        /*
+         * Aca entran ambos hijos y defino que el primer hijo imprima las
+         * las letras "pares" (a,c,e,g...) y el segundo las "impares" (b,d,e,.)
+         */
+        
         int j;
         
         if (i == 0){ 
+            
+            /*
+             * Este es el hijo 1 que imprime las letras pares.
+             */
             
             for(j = 0; j < 26 ; j = j + 2){
                 
@@ -66,6 +112,9 @@ int main(){
             
         }else{
             
+            /*
+             * Este es el hijo 2 que imprime las letras impares.
+             */
             
             for(j = 1; j < 26 ; j = j + 2){
                 
@@ -79,7 +128,13 @@ int main(){
             exit(0);
             
         }
+        
     }
 
     return 0;
 }
+
+
+/*******/
+/* FIN */
+/*******/
