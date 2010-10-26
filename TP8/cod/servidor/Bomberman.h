452bb9336b519256cd893ebe3f5d1380d0b1a168
rus.spes <rus.spes@4bd5a78b-6969-a388-ad65-09c7baf7c938>
2010-10-26 06:23:10 +0000

    [TP8] El servidor ahora acepta multiples jugadores y conexion "en caliente" de nuevos jugadores. TODO: Borrar jugadores cuando se desconectan.

--- TP8/cod/servidor/Bomberman.h
+++ TP8/cod/servidor/Bomberman.h
@@ -20,6 +20,8 @@
 
 //#include "../include/t_protocolo.h"
 
+#define JUGMAX 4
+
 // matriz de 9 X 37
 
 class Bomberman {
@@ -33,11 +35,17 @@ class Bomberman {
         
         int nuevoJugador();
 
-        Jugador& getJugador(int jugador);
+        void sendEscenario( unsigned jugador );
+
+        Jugador& getJugador( int jugador );
 
         unsigned getNumJugadores();
 
-        int update(t_protocolo data);
+        t_protocolo procesarAccion( t_protocolo data );
+
+        int update( t_protocolo data );
+
+        t_protocolo eliminarJugador( unsigned jugador );
 
         int clockTick();
 
@@ -85,37 +93,134 @@ void Bomberman :: activar( long int puerto ){
 int Bomberman :: nuevoJugador(){
     Connection<char> playerCon;
     unsigned numCon = 0;
+    unsigned numJugador = 0;
 
-    numCon = Socket.Accept();
+    numCon = this->Socket.Accept();
 
-    playerCon = Socket.getConn( numCon );
+    playerCon = this->Socket.getConn( numCon );
 
     Coordenada coord(0,0);
-    
-    Jugador Player( this->VidaInicial,
+
+    /*Saco numero de jugador*/{
+        unsigned i = 0;
+        
+        while( i < Jugadores.size() &&
+               i < JUGMAX &&
+               ! Jugadores[i].eliminado() ) i = i + 1;
+
+        if ( i >= JUGMAX )
+            return -1;
+        else
+            numJugador = i;
+    }   
+
+    Jugador Player( numJugador,
+                    this->VidaInicial,
                     coord,
                     this->MaxBombInicial,
                     playerCon );
 
-    Jugadores.push_back( Player );
+    if ( numJugador == Jugadores.size() ){
+        
+        this->Jugadores.push_back( Player );
+        
+    } else {
+        
+        this->Jugadores[numJugador] = Player;
+        
+    }
+
+    this->sendEscenario( numJugador );
     
-    return Jugadores.size() - 1;
+    return (int) numJugador;
+}
+
+void Bomberman :: sendEscenario( unsigned jugador ){
+    unsigned i = 0;
+    t_protocolo enviar;
+    
+    for( i = 0 ; i < Jugadores.size() ; i++ ){
+        enviar.id = 'j';
+        enviar.posicion = i;
+        enviar.x = Jugadores[i].getPosicion().get_x();
+        enviar.y = Jugadores[i].getPosicion().get_y();
+        
+        Jugadores[jugador].send(enviar);
+        
+    }
+
+    enviar.id = 'i';
+    enviar.posicion = 0;
+    enviar.x = jugador;
+    enviar.y = 0;
+
+    Jugadores[jugador].send(enviar);
+
 }
 
 Jugador& Bomberman :: getJugador(int jugador){
-    return Jugadores[ jugador ];
+    return this->Jugadores[ jugador ];
 }
 
 unsigned Bomberman :: getNumJugadores(){
-    return Jugadores.size();
+    return this->Jugadores.size();
+}
+
+t_protocolo Bomberman :: procesarAccion( t_protocolo recibido ){
+    t_protocolo enviar;
+    
+    unsigned jugador = recibido.posicion;
+
+    switch( recibido.x ){
+        case 'w':
+            this->Jugadores[ jugador ].moverArriba();
+            break;
+
+        case 's':
+            this->Jugadores[ jugador ].moverAbajo();
+            break;
+
+        case 'd':
+            this->Jugadores[ jugador ].moverDerecha();
+            break;
+
+        case 'a':
+            this->Jugadores[ jugador ].moverIzquierda();
+            break;
+
+    }
+
+    enviar.id = 'j';
+    enviar.posicion = jugador;
+    enviar.x = this->Jugadores[ jugador ].getPosicion().get_x();
+    enviar.y = this->Jugadores[ jugador ].getPosicion().get_y();
+
+    return enviar;
+
 }
 
 int Bomberman :: update( t_protocolo data ) {
 
     for( unsigned i = 0 ; i < Jugadores.size() ; i++ )
-        Jugadores[i].send( data );
+        if( ! this->Jugadores[i].eliminado() )
+            this->Jugadores[i].send( data );
     
     return 0;
+}
+
+t_protocolo Bomberman :: eliminarJugador( unsigned jugador ){
+    t_protocolo enviar;
+    
+    if( this->Jugadores.size() - 1 >= jugador ){
+        this->Jugadores[ jugador ].eliminar();
+    }
+
+    enviar.id = 'j';
+    enviar.posicion = jugador;
+    enviar.x = -1;
+    enviar.y = 0;
+
+    return enviar;
 }
 
 int Bomberman :: clockTick(){
