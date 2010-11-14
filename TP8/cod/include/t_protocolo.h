c36f72a605a21c138fe5e81319ff3dd216188712
rus.spes <rus.spes@4bd5a78b-6969-a388-ad65-09c7baf7c938>
2010-11-14 08:52:12 +0000

    [TP8] Includes: Divido en .h y .cpp.

--- TP8/cod/include/t_protocolo.h
+++ TP8/cod/include/t_protocolo.h
@@ -1,6 +1,26 @@
 #ifndef T_PROTOCOLO_H_
 #define T_PROTOCOLO_H_
 
+
+// -------------------------- define para memoria compartida --------------------------- //
+// trafico de datos:
+//                      SAC : servidor a cliente
+//                      CAS : cliente a servidor
+
+#define SERVIDOR   0
+#define CLIENTE    1
+
+#define S_INICIO_SERV 0
+#define S_INICIO_CLI  1
+
+#define S_BLOCK_CLI_SAC   2
+#define S_BLOCK_SERV_SAC  3
+
+#define S_BLOCK_CLI_CAS   4
+#define S_BLOCK_SERV_CAS  5
+
+// -------------------------- define para memoria compartida --------------------------- //
+
 typedef struct
 {
   char id;
