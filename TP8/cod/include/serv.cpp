6ce59101df76db2f1e0465cf2ebff1b695b9995f
facu22.14@gmail.com <facu22.14@gmail.com@4bd5a78b-6969-a388-ad65-09c7baf7c938>
2010-11-16 04:49:15 +0000

    [TP8]: Memoria Compartida : *Cliente y Servidor de prueba funcionando en forma optima *nuevas constantes definidas *restauracion de clase connection que yo habia borrado por mal uso del svn *ultimas versiones de MemCompartida ( cierre correcto de memorias y semaforos )

--- TP8/cod/include/serv.cpp
+++ TP8/cod/include/serv.cpp
@@ -1,15 +1,50 @@
 #include <iostream>
 #include <stdio.h>
+#include <signal.h>
 
 #include "MemCompartida.h"
 
 
-
 using namespace std;
+
+MemCompartida MemCli;
+
+void finalizar(int iNumSen, siginfo_t  * info, void *ni)
+{
+    
+    MemCli.eliminarMemoriaCompartida( SERVIDOR );
+    
+    
+        
+    if( iNumSen == SIGUSR1 )
+    {
+        cout << endl << "El cliente ha terminado";
+    }
+    
+    cout << endl << "El servidor ha finalizado" << endl << endl ;
+    exit (0);
+}
+
 
 int main(int argc, const char *argv[])
 {
-    MemCompartida MemCli( SERVIDOR );
+    
+     struct sigaction term;
+
+    term.sa_sigaction = finalizar;
+    sigfillset( &term.sa_mask );
+    term.sa_flags = SA_SIGINFO | SA_NODEFER;
+    sigaction(SIGINT , &term, NULL);
+    sigaction(SIGPIPE , &term, NULL);
+    sigaction(SIGTERM , &term, NULL);
+    sigaction(SIGQUIT , &term, NULL);
+    sigaction(SIGABRT , &term, NULL);
+    sigaction(SIGSEGV , &term, NULL);  
+    sigaction(SIGUSR1 , &term, NULL);  
+    
+    
+    MemCli.CargarMemCompartida( SERVIDOR );
+    
     MemCli.esperarUsuario();
     
     
@@ -39,8 +74,15 @@ int main(int argc, const char *argv[])
     cout << endl << "Se ha enviado : "  << envio.x  <<  endl << "Se ha recibido : "  <<  recepcion.x << endl;
     
     //------------------------------------------------------------------------------------------------------  
+    
+     cout << endl << endl << "El servidor esperara envios del cliente "  ;
+    while ( ((int) recepcion.x) !=  -1)
+    {
+        MemCli.recibirDeCliente( recepcion );
+        cout << endl <<  "Se ha recibido : "  <<  recepcion.x << endl;
+    }
 
-     
+    cout << endl << "Fin de servidor" << endl;
     MemCli.eliminarMemoriaCompartida( SERVIDOR );
     return 0;
     
