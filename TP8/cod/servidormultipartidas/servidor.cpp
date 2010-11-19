82ff0da0e337471e634834c621ddf54fa622d0be
rus.spes <rus.spes@4bd5a78b-6969-a388-ad65-09c7baf7c938>
2010-11-19 08:44:09 +0000

    [TP8] Servidor: servidor.cpp comentado todo antes del main().

--- TP8/cod/servidormultipartidas/servidor.cpp
+++ TP8/cod/servidormultipartidas/servidor.cpp
@@ -1,3 +1,18 @@
+/*
+ * servidor.cpp
+ *
+ * Descripcion:
+ * Se encarga del manejo de la partida en si, sincroniza el uso de
+ * la clase Bomberman.h.
+ *
+ * Crea un servidor maestro que administra las conexiones entrantes
+ * y da inicio a las partidas. Cada partida es manejada por un
+ * servidor esclavo, hijo del maestro. Asi se tiene un proceso por
+ * partida en curso (esclavo) mas un proceso que administra y crea
+ * las nuevas partidas (maestro).
+ *
+ */
+
 #include <iostream>
 #include <fstream>
 #include <pthread.h>
@@ -15,30 +30,78 @@
 #include <signal.h>
 #include <queue>
 
+
 #include "Bomberman.h"
 
 Bomberman Servidor;
 bool timeOutActivo = false;
 
+/* Espera nuevos jugadores remotos */
 void * nuevosJugadoresRemotos(void * args);
+
+/* Espera un nuevo jugador local */
 void * nuevoJugadorLocal(void * args);
+
+/* Espera un proceso particular */
 void * pidWaiter(void * args);
+
+/* Recibe datos de un jugador */
 void * recver(void * args);
+
+/* Procesa los datos recibidos */
 void * procesador(void * args);
+
+/* Envia los datos procesados */
 void * sender(void * args);
+
+/* Controla el tiempo de vida de las bombas */
 void * bomb(void * args);
+
+/* Controla el tiempo de vida de explosiones */
 void * explosion(void * args);
+
+/* Temporizador de la partida */
 void * timer(void * args);
+
+/* Temopoirzador de inicio de partida */
 void * timeOut(void * args);
 
+/* Encola en QRecibido */
 void pushQRecibido(t_protocolo recibido);
+
+/* Encola en QEnviar */
 void pushQEnviar(t_protocolo enviar);
+
+/* Desencola de QRecibido */
 t_protocolo popQRecibido();
+
+/* Desencola de QEnviar */
 t_protocolo popQEnviar();
+
+/* Termina una partida particular */
 void end();
+
+/* Activa todos los threads */
 void activateThreads();
+
+/* Cancela todos los threads */
 void cancelThreads();
 
+/*
+ * Nota: si bien la formalidad indica que los comentarios
+ * en declaraciones de variables deben ser brebes y no
+ * superar una o dos lineas, me parecio interesante extender
+ * la longitud para asi poder dar un brebe resumen del
+ * funcionamiento del programa en estas declaraciones.
+ *
+ * Disfruten ;)
+ */
+
+/*
+ * Definicion de los pthread_t para los
+ * threads estaticos, o sea, los que no
+ * se usan para recibir datos de jugadores.
+ */
 pthread_t procesadorThread,
           senderThread,
           bombThread,
@@ -48,52 +111,180 @@ pthread_t procesadorThread,
           nuevosJugadoresRemotosThread,
           nuevoJugadorLocalThread;
 
+/*
+ * Cola de datos recibidos. Cada thread de recepcion (recver)
+ * recibe las teclas precionadas por el usuario y las encola
+ * en QRecibido.
+ */
 queue<t_protocolo> QRecibido;
+
+/*
+ * Cola de datos a enviar. El thread de envio de datos desencola
+ * de a un elemento de esta cola y lo envia a todos los jugadores
+ * excepto que id = 'i', en donde se lo enviara al numero de jugador
+ * que figure en x.
+ */
 queue<t_protocolo> QEnviar;
 
+/*
+ * Cola de numero de jugadores. Cada vez que un jugador se conecta
+ * se encola el numero en esta cola. Ese numero se usa lugo para
+ * crear los threads de recepcion.
+ */
 queue<int> QNumJugadores;
 
+/*
+ * pthread_t de threads de recepcion. Cuando se crea un nuevo thread
+ * de recepcion se agrega a esta estructura. En el primer dato se guarda
+ * el numero de jugador y en el segundo el thread que recibe datos de
+ * ese jugador.
+ */
 map< int, pthread_t > recvJugadores;
+
+/*
+ * pthread_t de threads de espera de partidas. Cuando se crea una nueva
+ * partida, tambien se crea un nuevo proceso, este proceso debe ser tratado
+ * y esperado por el padre. En el primer dato se guarda el PID del servidor
+ * hijo de la partida y en el segundo el pthread_t del thread que espera a
+ * dicho hijo (partida).
+ */
 map< int, pthread_t > pidWaiters;
 
+/* Almacenamiento temporal del pid del hijo */
 pid_t childPid;
+
+/*
+ * PID de la partida que tiene un jugador local. Como no se admite mas de
+ * un jugador local a nivel global, se almacena la partida en la que juega
+ * el jugador local para que cuando esta termine se habilite la recepcion
+ * de un nuevo jugador local.
+ */
 pid_t jugadorLocalPid;
 
+/*
+ * Indica si hay un jugador local en alguna partida. VER COMENTARIO ANTERIOR.
+ */
 bool hayJugadorLocal = false;
 
+/*
+ * Aca inician los semaforos y condiciones para sincronizar los threads.
+ * Cada semaforo y condicional cumple una funcion especifica dentro de la
+ * sincronizacion de los threads.
+ */
+
+/*
+ * Bloqueo de jugador local. Este bloqueo se usa para que solo haya un
+ * jugador local globalmente, cuando se recibe un nuevo jugador local
+ * el thread nuevoJugadorLocal() se bloquea bajo esta condicion hasta
+ * que no termine la partida que contiene al jugador local.
+ */
 pthread_mutex_t JugLocalMutex = PTHREAD_MUTEX_INITIALIZER;
 pthread_cond_t JugLocalCond = PTHREAD_COND_INITIALIZER;
 
+/*
+ * Bloqueo de la creacion de threads. Mas adelante se explica el porque
+ * de este bloqueo. VER: lugar de creacion de threads de recepcion.
+ */
 pthread_mutex_t CrearNuevoThreadMutex = PTHREAD_MUTEX_INITIALIZER;
 pthread_cond_t CrearNuevoThreadCond = PTHREAD_COND_INITIALIZER;
 
+/*
+ * Bloqueo de procesamiento. Bloquea el thread de procesamiento hasta
+ * que tenga algo que procesar en la cola QRecibido.
+ */
 pthread_mutex_t ProcesadorMutex = PTHREAD_MUTEX_INITIALIZER;
 pthread_cond_t ProcesadorCond = PTHREAD_COND_INITIALIZER;
 
+/*
+ * Bloqueo del thread de envio. Bloquea al thread de envio
+ * de datos hasta que tenga algo que enviar en QEnviar.
+ */
 pthread_mutex_t SenderMutex = PTHREAD_MUTEX_INITIALIZER;
 pthread_cond_t SenderCond = PTHREAD_COND_INITIALIZER;
 
+/*
+ * Bloqueo de bombas. Bloquea el thread de tiempo de vida de
+ * bombas hasta que tenga bombas que procesar.
+ */
 pthread_mutex_t BombMutex = PTHREAD_MUTEX_INITIALIZER;
 pthread_cond_t BombCond = PTHREAD_COND_INITIALIZER;
 
+/*
+ * Bloqueo de explosiones. Bloquea el thread de tiempo de vida
+ * de explosiones hasta que alguna bomba haya explotado y tenga
+ * explosiones que procesar.
+ */
 pthread_mutex_t ExplosionMutex = PTHREAD_MUTEX_INITIALIZER;
 pthread_cond_t ExplosionCond = PTHREAD_COND_INITIALIZER;
 
+/*
+ * Sincronizacion de clock. ClockStart* controla el inicio del
+ * reloj de la partida. ClockMutex controla que solo un thread
+ * modifique el clock al mismo tiempo. (Nota: esto no es necesario
+ * para este caso, pero se considera una buena practica ya que el
+ * clock es un recurso critico)
+ */
 pthread_mutex_t ClockMutex = PTHREAD_MUTEX_INITIALIZER;
 pthread_mutex_t ClockStartMutex = PTHREAD_MUTEX_INITIALIZER;
 pthread_cond_t ClockStartCond = PTHREAD_COND_INITIALIZER;
 
+/*
+ * Bloqueo de inicio de time out o tiempo de inico de partida.
+ * Se usa para mantener bloqueado al thread que controla el inicio
+ * de la partida hasta que se conecte el primer jugador.
+ */
 pthread_mutex_t TimeOutMutex = PTHREAD_MUTEX_INITIALIZER;
 pthread_cond_t TimeOutStartCond = PTHREAD_COND_INITIALIZER;
 
+/*
+ * Bloqueo de fin de inicio de partida (no me volvi loco, es asi).
+ * Se utiliza para indicar que el inicio de partida finalizo. Esto
+ * desencadena los sucesos que crean una nueva partida con los
+ * jugadores conectados hasta el momento.
+ */
 pthread_mutex_t TimeOutEndMutex = PTHREAD_MUTEX_INITIALIZER;
 pthread_cond_t TimeOutEndCond = PTHREAD_COND_INITIALIZER;
 
+/*
+ * Semaforos para la sincronizacion de colas QRecibido y
+ * QEnviado. Para que cualquier operacion sobre las colas
+ * sea atomica, se bloquea el semaforo cada vez que se quiere
+ * modificar o leer datos de las colas.
+ */
 pthread_mutex_t QRecibidoMutex = PTHREAD_MUTEX_INITIALIZER;
 pthread_mutex_t QEnviarMutex = PTHREAD_MUTEX_INITIALIZER;
 
+/*
+ * Semaforo para la sincronizacion de nuevos jugadores. Como hay
+ * 2 threads esperando jugadores (local y remoto) se debe asegurar
+ * que solo uno a la vez pueda agregar un nuevo jugador, de lo
+ * contrario, el resultado es inesperado.
+ */
 pthread_mutex_t NuevoJugadorMutex = PTHREAD_MUTEX_INITIALIZER;
 
+
+
+/* void cSIGPIPE(int iNumSen, siginfo_t *info, void *ni)
+ *
+ * Descripcion:
+ * Esta funcion maneja la señal SIGPIPE. Esta señal indica que un
+ * pipe se ha roto, y en este caso nos dice que se perdio una
+ * conexion. Esta funcion busca el/los jugadores que se hayan
+ * desconectado a travez del metodo CloseClosed de la clase
+ * Bomberman.
+ *
+ * Recibe:
+ * int iNumSen
+ * siginfo_t *info
+ * void *ni
+ *
+ * Nota: no se explican estos parametros porque no se usan en
+ * este programa.
+ *
+ * Retorna:
+ * void.
+ *
+ */
 void cSIGPIPE(int iNumSen, siginfo_t *info, void *ni){
     cerr << "Pipe Roto." << endl;
 
@@ -101,7 +292,18 @@ void cSIGPIPE(int iNumSen, siginfo_t *info, void *ni){
 
     cout << "Un jugador se ha desconectado inesperadamente!" << endl;
     cout << "Eliminando jugadores cerrados.......";
+
+    /*
+     * Duerme un segundo para asegurarse de que se le intente mandar el
+     * tiempo a todos los juegadores y asi poder conocer cual es el
+     * causante de esta señal, o sea, cual se desconecto.
+     *
+     * Nota: Hay formas mas elegantes de hacer esto, pero esta funciona
+     * de lo mas bien =)
+     */
     sleep(1);
+
+
     enviar = Servidor.CloseClosed();
 
     while( !enviar.empty() ){
@@ -112,6 +314,27 @@ void cSIGPIPE(int iNumSen, siginfo_t *info, void *ni){
     cout << "[OK]" << endl;
 }
 
+
+/* void cTERMPadre(int iNumSen, siginfo_t *info, void *ni)
+ *
+ * Descripcion:
+ * Esta funcion maneja la señal SIGTERM en el servidor maestro.
+ * Se encarga de terminar todos los esclavos de forma correcta,
+ * cerrar todos los threads, cerrar las areas de memoria compartida,
+ * eliminar los semaforos y cerrar el socket de escucha.
+ *
+ * Recibe:
+ * int iNumSen
+ * siginfo_t *info
+ * void *ni
+ *
+ * Nota: no se explican estos parametros porque no se usan en
+ * este programa.
+ *
+ * Retorna:
+ * void.
+ *
+ */
 void cTERMPadre(int iNumSen, siginfo_t *info, void *ni){
 	map<int, pthread_t>::iterator it;
 
@@ -119,7 +342,14 @@ void cTERMPadre(int iNumSen, siginfo_t *info, void *ni){
 	for ( it = pidWaiters.begin() ; it != pidWaiters.end() ; it++ ){
 		kill(SIGTERM, it->first);
 		cout << "Terminando proceso...." << "Pid: " << it->first;
+
+		/*
+		 * Como cada proceso tiene asignado un thread que espera su terminacion,
+		 * tiene sentido esperar al thread que espera al proceso en lugar de al
+		 * proceso en si.
+		 */
 		pthread_join( it->second, NULL );
+
 		cout << "[TERMINADO]" << endl;
 	}
 
@@ -147,6 +377,25 @@ void cTERMPadre(int iNumSen, siginfo_t *info, void *ni){
     exit(0);
 }
 
+/* void cTERMHijo(int iNumSen, siginfo_t *info, void *ni)
+ *
+ * Descripcion:
+ * Esta funcion maneja la señal SIGTERM en el servidor esclavo.
+ * Cierra todos los threads activos y cierra las conexiones puntuales
+ * con los clientes que estuvieran conectados a esa partida.
+ *
+ * Recibe:
+ * int iNumSen
+ * siginfo_t *info
+ * void *ni
+ *
+ * Nota: no se explican estos parametros porque no se usan en
+ * este programa.
+ *
+ * Retorna:
+ * void.
+ *
+ */
 void cTERMHijo(int iNumSen, siginfo_t *info, void *ni){
 	end();
 	exit(-1);
